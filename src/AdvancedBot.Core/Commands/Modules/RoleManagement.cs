34bf05d85080410d5d6d092dfdb3ba074c39e12f
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-04-29 14:25:37 +0200

    Implement addrole / removerole

--- src/AdvancedBot.Core/Commands/Modules/RoleManagement.cs
+++ src/AdvancedBot.Core/Commands/Modules/RoleManagement.cs
@@ -1,14 +1,26 @@
-using AdvancedBot.Core.Commands;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
 
 namespace AdvancedBot.Core.Commands.Modules
 {
     public class RoleManagement : CustomModule
     {
-        [Command("addrole")]
+        [Command("addrole")][Alias("ar")]
         public async Task AddRole(SocketGuildUser user, SocketRole role)
         {
             await user.AddRoleAsync(role);
-            await ReplyAsync("Successfully added role **{role.Name}** to **{user.Username}**");
+            await ReplyAsync($"Successfully added role **{role.Name}** to **{user.Username}**.");
+        }
+
+        [Command("removerole")][Alias("rr")]
+        public async Task RemoveRole(SocketGuildUser user, SocketRole role)
+        {
+            if (!user.Roles.Contains(role)) throw new Exception("User doesn't have this role");
+            await user.RemoveRoleAsync(role);
+            await ReplyAsync($"Successfully removed **{role.Name}** from **{user.Username}**.");
         }
     }
 }
